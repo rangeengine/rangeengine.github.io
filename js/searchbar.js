135a12d091838788808332606a22f212bee4dc01
SpectreGameDev <110704043+SpectreGameDev@users.noreply.github.com>
2022-11-30 20:03:24 -0300

    Major Fixes Store 1.1 WIP
    -Search engine now reads the title and description of the product properly

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,9 +1,8 @@
 function searchBar() {
   let input = document.getElementById('searchbar').value
-  input=input.toLowerCase();
+  input = input.toLowerCase().replace(/^\s+|\s+$/g, '');
   let x = document.getElementsByClassName('artigos');
   let y = document.getElementsByClassName('namepdt');
-  let z = document.getElementsByClassName('descpdt');
 
   for (i = 0; i < y.length; i++) {
     if (!y[i].innerHTML.toLowerCase().includes(input)) {
@@ -13,37 +12,24 @@ function searchBar() {
       x[i].style.display="inline-flex";
     }
   }
-  for (i = 0; i < z.length; i++) {
-    if (!z[i].innerHTML.toLowerCase().includes(input)) {
-      x[i].style.display="none";
-    }
-    else {
-      x[i].style.display="inline-flex";
-    }
-  }
 }
 function writeTSB() {
-  var defaultText = "Template";
   var searchBox = document.getElementById("searchbar");
-  searchBox.value = defaultText;
+  searchBox.value = "Template";
 }
 function writeASB() {
-  var defaultText = "Addon";
   var searchBox = document.getElementById("searchbar");
-  searchBox.value = defaultText;
+  searchBox.value = "Addon";
 }
 function writeSSB() {
-  var defaultText = "Script";
   var searchBox = document.getElementById("searchbar");
-  searchBox.value = defaultText;
+  searchBox.value = "Script";
 }
 function writeSHSB() {
-  var defaultText = "Shader";
   var searchBox = document.getElementById("searchbar");
-  searchBox.value = defaultText;
+  searchBox.value = "Shader";
 }
 function writeTUSB() {
-  var defaultText = "Tutorial";
   var searchBox = document.getElementById("searchbar");
-  searchBox.value = defaultText;
+  searchBox.value = "Tutorial";
 }
