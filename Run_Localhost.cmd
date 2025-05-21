bff5d3768bdf99ad611a69d68038ceef300c0ea7
hexterminal <general.defacing593@passinbox.com>
2025-05-21 20:18:32 +0530

    Update Run_Localhost.cmd

--- Run_Localhost.cmd
+++ Run_Localhost.cmd
@@ -1,6 +1,8 @@
 @echo off
-FOR /F "tokens=*" %%i IN ('where python') DO SET PYTHON_PATH=%%i
-FOR %%i IN ("%PYTHON_PATH%") DO SET PYTHON_DIR=%%~dpi
+
+SET PATH=C:\Users\\AppData\Local\Programs\Python\Python312
 SET PY_SCRIPT="Run_Localhost.py"
-"%PYTHON_PATH%" %PY_SCRIPT%
+
+python %PY_SCRIPT%
+
 pause
